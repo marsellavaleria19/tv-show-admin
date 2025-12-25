9832974e09a17bb9514b9882f5317808b5ca6b21
marsellavaleria19 <marsellavaleria19@gmail.com>
2025-12-25 08:52:16 +0700

    feat : add page management data

---
+++ pages/management-data/[id]/index.vue
@@ -0,0 +1,31 @@
+<template>
+   <DetailPage title="Detail Acara TV" :detail="dataDetail" link="/"/>
+</template>
+
+<script setup lang="ts">
+import { useStoreLayoutItem } from '~/store/layout-item';
+import { useStoreFormTvShow } from '~/store/tv-show';
+
+
+const route = useRoute()
+
+// --- STORE ---
+const storeFormTvShow = useStoreFormTvShow()
+const storeLayoutItem = useStoreLayoutItem()
+
+// --- COMPUTED ---
+const dataDetail = computed( ()=>{
+  return storeFormTvShow.detailTvShow
+})
+
+onMounted(() => {
+  storeFormTvShow.getAllDataCategories()
+  storeFormTvShow.getDataTvShowById(route.params.id as string)
+  storeLayoutItem.setNavigations([
+    {
+      label: 'Management Data',
+      link: '/',
+    },
+  ])
+})
+</script>
---
+++ pages/management-data/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <ListPage :list-header="tvShowTableHeaders" :list-data="listData" :page-data="pagination" row-key="id_show" link="/management-data"/>
+</template>
+
+<script setup lang="ts">
+import { useStoreLayoutItem } from '~/store/layout-item';
+import { tvShowTableHeaders } from '~/constant/data-tv-show';
+import { useStoreFormTvShow } from '~/store/tv-show';
+
+// definePageMeta({
+//   layout: 'main-layout'
+// })
+
+const storeFormTvShow = useStoreFormTvShow()
+const storeLayoutItem = useStoreLayoutItem()
+
+// --- COMPUTED ---
+const listData = computed( ()=>{
+  return storeFormTvShow.listDataTvShow
+})
+
+const pagination = computed(()=>{
+  return storeFormTvShow.pagination
+})
+
+onMounted(() => {
+  storeFormTvShow.getAllDataCategories()
+  storeFormTvShow.getAllDataTvShow()
+  storeLayoutItem.setNavigations([
+    {
+      label: 'Management Data',
+      link: '/management-data',
+    },
+  ])
+})
+</script>
