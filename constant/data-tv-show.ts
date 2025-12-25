35546586a2b3596ec7dd4e6c1f51c2855179656c
marsellavaleria19 <marsellavaleria19@gmail.com>
2025-12-25 08:51:10 +0700

    feat : add constant data tv show & menu list

---
+++ constant/data-tv-show.ts
@@ -0,0 +1,14 @@
+export const tvShowTableHeaders = [
+  { key: "id_show", text: "Kode", sortable: true },
+  { key: "name_show", text: "Nama", sortable: true },
+  { key: "category_name", text: "Kategori" },
+  { key: "premiered_at_format", text: "Tanggal Tayang", sortable: true },
+  { key: "action", text: "Aksi" },
+];
+
+export const tvShowSyncTableHeaders = [
+  { key: "id_show", text: "Kode", sortable: true },
+  { key: "name_show", text: "Nama", sortable: true },
+  { key: "category_name", text: "Kategori" },
+  { key: "last_synced_format", text: "Tanggal Sikronisasi", sortable: true },
+];
---
+++ constant/menu-list.ts
@@ -0,0 +1,22 @@
+
+
+export const menu = [
+  {
+    name: "Dashboard",
+    path: "/",
+    arrow: false,
+    icon: "HomeIcon",
+  },
+  {
+    name: "Management Data",
+    path: "/management-data",
+    arrow: false,
+    icon: "DatabaseIcon",
+  },
+  {
+    name: "Synchronize",
+    path: "/synchronize",
+    arrow: false,
+    icon: "SettingsIcon",
+  },
+];
