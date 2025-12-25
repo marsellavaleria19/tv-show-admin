76371d9f230f2a761d9e5b550651f207c3c185ed
marsellavaleria19 <marsellavaleria19@gmail.com>
2025-12-25 08:16:44 +0700

    feat : add chart component

---
+++ components/Chart/Column/index.vue
@@ -0,0 +1,72 @@
+<template>
+    <ApexChart type="bar" :options="chartOptions" :series="chartSeries" height="350" />
+</template>
+
+<script setup lang="ts">
+import ApexChart from "vue3-apexcharts";
+import { ref } from "vue";
+
+const props = defineProps<{
+    dataChart:DataChart[]
+  }>()
+
+interface DataChart {
+    label:string,
+    value:number
+  }
+const labels = computed(() =>
+  props.dataChart?.length ? props.dataChart.map(item => item.label) : []
+)
+
+const values = computed(() =>
+  props.dataChart?.length ? props.dataChart.map(item => item.value) : []
+)
+
+
+// Data chart
+const chartSeries = computed(() => [
+  {
+    name: "Total Shows",
+    // Pastikan jumlah data di sini sama dengan jumlah categories di atas
+    data: values.value
+  }
+]);
+
+const chartOptions = computed(() => ({
+  chart: { id: "myChart", type: "bar" },
+  xaxis: {
+    // Pastikan mengambil dari chartData yang sama
+    categories: labels.value || []
+  },
+  dataLabels: {
+    enabled: false
+  },
+})) as any;
+
+// Opsi chart
+// const options = ref({
+//   chart: {
+//     id: "myChart",
+//     type: "bar"
+//   },
+//   plotOptions: {
+//     bar: {
+//       horizontal: false
+//     }
+//   },
+//   dataLabels: {
+//     enabled: false
+//   },
+//   xaxis: {
+//    categories: labels || []
+//   },
+//   yaxis: {
+//     title: {
+//       text: "Total Shows"
+//     }
+//   },
+//   tooltip: {
+//     enabled: true
+//   }
+// }) as any;
+</script>
---
+++ components/Chart/Pie/index.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+  const props = defineProps<{
+    dataChart:DataChart[]
+  }>()
+
+ 
+  interface DataChart {
+    label:string,
+    value:number
+  }
+const labels = computed(() =>
+  props.dataChart?.length ? props.dataChart.map(item => item.label) : []
+)
+const series = computed(() =>
+  props.dataChart?.length ? props.dataChart.map(item => item.value) : []
+)
+
+const options = computed(() => ({
+  chart: { type: 'pie' },
+  labels: labels.value
+}))
+</script>
+
+<template>
+    <VueApexCharts class="w-[500px] h-[500px]" type="pie" :series="series" :options="options" />
+</template>
