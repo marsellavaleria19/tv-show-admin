5eecf1021ade2cc4411b98aef9f4bf2aa8698437
marsellavaleria19 <marsellavaleria19@gmail.com>
2025-12-25 08:46:40 +0700

    feat : add component page

---
+++ components/Page/Header/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1 class="text-[20px] text-[#022B55] pb-4 font-bold">{{ props.title }}</h1>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  title: string;
+}>();
+</script>
---
+++ components/Page/Panel/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="bg-white border border-[#022B55] rounded-lg w-full h-full">
+    <div class="p-6">
+      <slot />
+    </div>
+  </div>
+</template>
