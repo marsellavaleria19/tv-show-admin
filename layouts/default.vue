2487d4de9258e271f6fb71561c244430b6ead0f7
marsellavaleria19 <marsellavaleria19@gmail.com>
2025-12-25 08:51:32 +0700

    feat : add default layouts

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="flex bg-[#EDEFF3]">
+      <LayoutSidebar class="fixed left-0 top-0 h-screen w-[325px] bg-[#022B55]" />
+      <div class="h-full w-full">
+        <LayoutHeader :navigations="navigations" :profile="profile"/>
+        <div class="max-w-screen-xl px-6 py-6 2xl:max-w-full 2xl:px-10">
+          <NuxtPage />
+          <Toaster position="top-right" rich-colors/>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+// call store
+import { storeToRefs } from "pinia";
+import { useStoreLayoutItem } from "~/store/layout-item";
+import { Toaster } from 'vue-sonner'
+import logo from '@/assets/image/user.jpg'
+
+const store = useStoreLayoutItem();
+const { navigations } = storeToRefs(store);
+const profile = reactive({image:'',name:''})
+profile.image = logo
+profile.name = 'Admin'
+</script>
